36c0aecbad24a2656c17fa61ce38daf39e2badb2
Chris Kapusta <ckapusta@outlook.com>
2020-05-08 12:02:15 -0500

    First Commit

---
+++ exercise-stackoverflow/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace exercise_stackoverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stacklogic = new StackLogic();
+
+            while (true)
+            {
+                Console.WriteLine(System.Environment.NewLine + stacklogic.PostTitle() + System.Environment.NewLine + stacklogic.PostContent() + System.Environment.NewLine);
+                Console.WriteLine("Current vote: " + stacklogic.PostScore);
+                Console.WriteLine("{0} Upvotes and {1} Downvotes", stacklogic.GetUpVote(), stacklogic.GetDownVote());
+                Console.WriteLine("Please select an option below:");
+                Console.WriteLine("1. Upvote Post: ");
+                Console.WriteLine("2. Downvote Post: ");
+                Console.WriteLine("3. Exit");
+                Console.Write("Selection: ");
+                var input = Console.ReadLine();
+
+                if ((String.IsNullOrWhiteSpace(input)) || (Convert.ToInt32(input) == 3))
+                    break;
+
+                else if (Convert.ToInt32(input) == 1)
+                {
+                    stacklogic.UpVote();
+                }
+
+                else
+                {
+                    stacklogic.DownVote();
+                }
+
+
+
+            }
+        }
+    }
+}
---
+++ exercise-stackoverflow/StackLogic.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace exercise_stackoverflow
+{
+    class StackLogic
+    {
+        /*
+         * 
+         * set private variables for the class 
+         * 
+         * 
+         */
+        private int _upvote;
+        private int _downvote;
+        private string _posttitle;
+        private string _postcontent;
+
+
+        //Class Constructor, used here to build the post content for demo pourposes
+        public StackLogic()
+        {
+            _posttitle = "This is my post title!";
+            _postcontent = "I'm learning to use classes, I hope this works!";
+        }
+
+        //Keep track of the overall post score.  Protect the Set so it cannot be changed outside of the class
+        public int PostScore { get; private set; }
+        
+        //returns post title
+        public string PostTitle()
+        {
+            return _posttitle;
+        }
+
+        //returns post content
+        public string PostContent()
+        {
+
+            return _postcontent;
+        }
+
+        //Processes an upvote
+        public void UpVote()
+        {
+
+            _upvote += 1;
+            PostScore += 1;
+
+
+        }
+
+        //processes a downvote
+        public void DownVote()
+        {
+            _downvote += 1; 
+            PostScore -= 1;
+
+
+        }
+        //Returns the current upvote count
+        public int GetUpVote()
+        {
+            return _upvote;
+
+        }
+
+        //Returns the current downvote count
+        public int GetDownVote()
+        {
+            return _downvote;
+
+        }
+
+    }
+}
